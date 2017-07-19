21a81300fde0c0374e36210b5f050a7c1bfd1162
s4lmon <mail@hasanamin.com>
2017-07-19 12:13:11 +0100

    clean up unused sections of code

--- diamondsolve.f90
+++ diamondsolve.f90
@@ -1,23 +1,17 @@
-!x_array firxed
-!todo psi correction.
 
 module diamondsolve
   use constants
-
   implicit none
-
+real(kind=dp),parameter :: source = 1.0_dp
 contains
-  subroutine dsolve(sigma,disc,length,source,psi_array,x_array)
-    real(kind=dp) :: psi,sigma,delta,mu,psi_half,length,source
+  subroutine dsolve(sigma,disc,length,smultiply,q_array,psi_array,x_array,alpha)
+    real(kind=dp) :: psi,sigma,delta,mu,psi_half,length,smultiply,alpha,excess
+
     integer :: disc,ii,temp
-    real(kind=dp),dimension(:),allocatable :: psi_array,x_array,q_array
+    real(kind=dp),dimension(:) :: psi_array,x_array,q_array
 
-    delta=length/(disc)
+    delta=length/dble(disc)
     mu=1
-
-    allocate(psi_array(disc))
-    allocate(x_array(disc))
-    allocate(q_array(disc))
 
     q_array=0
 
@@ -27,26 +21,22 @@ contains
     x_array=delta/2.0_dp
       do ii = 2,disc
         x_array(ii)=x_array(ii-1)+delta
+
       end do
 
       do  ii=1,disc
-        !psi_half is half step BEFORE current evaluation of psi
+
 
         psi=((1.0_dp+(sigma*delta)/(2.0_dp*abs(mu)))**(-1))*(psi_half+((delta*q_array(ii))/(2.0_dp*mu)))
         psi_array(ii)=psi
 
-        print*,'psi::x',psi,x_array(ii)
+
         psi_half=2*psi-psi_half
-        !print*,'psi_half',psi_half
 
       end do
 
-      ! output data to a file
-      open(100, file="data1.dat", status="unknown")
-        do ii=1,disc
-          write(100,*) x_array(ii), psi_array(ii)
-        end do
-      close(100)
+  alpha=psi_half
+
   end subroutine
 
 
